9449ee70024b71745230632da3b46cc0e005d997
carl-alvarez <99760960+carl-alvarez@users.noreply.github.com>
2023-04-02 21:59:09 -0300

    Initial commit

---
+++ Assets/Script/camMove.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camMove : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float rotateSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckMovement();
+        CheckRotation();
+    }
+
+    void CheckMovement()
+    {
+        var xMove = transform.right * (Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime);
+        var zMove = transform.forward * (Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime);
+        var move = xMove + zMove;
+
+        transform.position += move;
+    }
+
+    void CheckRotation()
+    {
+        var rotation = Input.GetAxisRaw("Mouse X") * rotateSpeed * Time.deltaTime * 500;
+
+        transform.Rotate(0f, rotation, 0f);
+    }
+}
